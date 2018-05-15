c958aee0418d3334fe91d6fde5a14c76f7e4a536
Ryan Kretch <rkretch91@gmail.com>
2018-05-15 22:17:50 +0200

    admin panel added for experts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+
+    root to: "users#index"
+  end
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
