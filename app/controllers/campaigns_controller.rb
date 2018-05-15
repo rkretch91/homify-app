87d0fe2345c82230f2c0e74a58678691b9abcccb
Ryan Kretch <rkretch91@gmail.com>
2018-05-16 00:06:41 +0200

    users can now create campaigns and are redirected to show page

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,2 +1,30 @@
 class CampaignsController < ApplicationController
+  def index
+    @campaigns = Campaign.all
+  end
+
+  def show
+    @campaign = Campaign.find(params[:id])
+  end
+
+  def new
+    @campaign = Campaign.new
+    @user = current_user
+  end
+
+  def create
+    @campaign = Campaign.new(params_campaign)
+    @user = current_user
+    @campaign.user = @user
+    if @campaign.save
+      redirect_to campaign_path(@campaign), notice: "Campaign Added to System"
+    else
+      render :new
+    end
+  end
+  private
+
+  def params_campaign
+    params.require(:campaign).permit(:title, :tags, :estimated_duration, :user_id)
+  end
 end
