d3ece431a6d1f901a7e9506fc737c836b9e9ce8e
Ryan Kretch <rkretch91@gmail.com>
2018-05-16 02:04:36 +0200

    todo lists working and displaying on campaign show pages

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -12,13 +12,17 @@ class TodosController < ApplicationController
     @todo = Todo.new(params_todo)
     @campaign = Campaign.find(params[:campaign_id])
     @todo.campaign = @campaign
-    @todo.save
+    if @todo.save
+      redirect_to campaign_path(@campaign), notice: "Todo List Posted"
+    else
+      render :new
+    end
   end
 
   private
 
   def params_todo
-    params.require(:todo).permit(:items, :campaign_id)
+    params.require(:todo).permit(:items)
   end
 
 
